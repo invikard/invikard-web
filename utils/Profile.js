ada73f677effa1822dbe5f8246f315b37ab2df8b
hynra <hynra@yahoo.com>
2020-01-25 20:16:13 +0700

    parse profile & nodes

--- utils/Profile.js
+++ utils/Profile.js
@@ -1,6 +1,9 @@
 import fetch from 'isomorphic-unfetch';
+import {validateAll} from "./parse_handler";
 
 const instagramUrl = 'https://www.instagram.com/';
+const instagramGraphQL = 'https://www.instagram.com/graphql/query/';
+
 export const validateProfile = async (userName) => {
     try {
         const profileUrl = `${instagramUrl}${userName}/?__a=1`;
@@ -56,8 +59,33 @@ export const validateProfile = async (userName) => {
     }
 };
 
-export const loadAndValidateMedia = async (id) => {
+export const loadAndValidateMedia = async (profile) => {
     try {
+        const {id, hasNext, end_cursor} = profile;
+        const fetchUrl = `${instagramGraphQL}?query_hash=472f257a40c653c64c666ce877d59d2b&variables={"id":"${id}","first":100,"after":"${end_cursor}"}`;
+        const res = await fetch(fetchUrl, {
+            method: 'GET',
+        });
+        const status = res.status;
+        if (status !== 200) throw new Error('Something went wrong');
+        let media = await res.json();
+        media = media.data.user.edge_owner_to_timeline_media.edges;
+        let nodes = [];
+        for (let i = 0; i < media.length; i++) {
+            let node = {
+                image: media[i].node.display_url,
+                caption: (media[i].node.edge_media_to_caption.edges.length > 0) ?
+                    media[i].node.edge_media_to_caption.edges[0].node.text.replace("\n", " \n") : "No Caption",
+                shortcode: media[i].node.shortcode,
+                location: media[i].node.location,
+                tagged_user: media[i].node.edge_media_to_tagged_user,
+                comments: media[i].node.edge_media_to_comment
+            };
+
+            nodes.push(node);
+        }
+
+        return validateAll({nodes, profile});
 
     }catch (e) {
         throw e;
---
+++ utils/parse_handler.js
@@ -0,0 +1,149 @@
+import * as getUrls from 'get-urls';
+import * as moment from 'moment'
+
+export const validateAll = (data) => {
+    const { nodes, profile } = data;
+    const requiredHastags = ['#invikardvideo', '#invikarddate', '#invikardplace',
+        '#invikardbride', '#invikardgroom', '#invikardbanner', '#invikardgallery', "#invikardstory", "#invikardcomments"];
+
+    // validate all components
+    let valids = {
+        is_video: false,
+        is_date: false,
+        is_place: false,
+        is_banner: false,
+        is_map_url: false,
+        is_video_url: false,
+        is_bride: false,
+        is_groom: false,
+        is_config: false,
+        is_comment: false,
+        gallery: [],
+        stories: []
+    };
+
+    console.log(nodes.length);
+    nodes.map(node => {
+        let strs = node.caption.split(" ");
+        let ht = "none";
+        strs.map(s => {
+            if (s.includes("#")) {
+                ht = s.toLowerCase();
+            }
+        });
+
+        const urlRegex = /(https?:\/\/[^ ]*)/;
+
+        switch (ht) {
+            case requiredHastags[0]:
+                valids.is_video = true;
+                let videoUrl = node.caption.match(urlRegex)[1];
+                node.video_url = videoUrl;
+                node.video_id = videoUrl.split("/");
+                node.video_id = node.video_id[node.video_id.length-1];
+                valids.video = node;
+                if (videoUrl) {
+                    valids.is_video_url = true;
+                }
+                break;
+            case requiredHastags[1]:
+                node.caption = node.caption.toLowerCase();
+                node.caption = node.caption.replace(ht, "").trim();
+                const date = moment(node.caption, 'DD-MM-YYYY', true);
+                if(date !== null) {
+                    let startTime = date.add(2, 'h');
+                    startTime = startTime.format('YYYYMMDDTHHmmss')+'Z';
+                    let endTime = date.add(14, 'h');
+                    endTime = endTime.format('YYYYMMDDTHHmmss')+'Z';
+                    node.start = startTime;
+                    node.end = endTime;
+                    valids.is_date = true;
+                    node.date = date;
+                }
+
+                valids.date = node;
+                break;
+            case requiredHastags[2]:
+                let placeUrl = node.caption.match(urlRegex)[1];
+                node.map_url = placeUrl;
+                if (placeUrl) {
+                    node.caption = node.caption.replace(placeUrl, "");
+                    valids.is_map_url = true;
+                    valids.is_place = true;
+                }
+                valids.place = node;
+                break;
+            case requiredHastags[3]:
+                valids.is_bride = true;
+                valids.bride = node;
+                if (node.tagged_user.edges.length > 0) valids.is_bride = true;
+                break;
+            case requiredHastags[4]:
+                valids.is_groom = true;
+                valids.groom = node;
+                if (node.tagged_user.edges.length > 0) valids.is_groom = true;
+                break;
+            case requiredHastags[5]:
+                valids.is_banner = true;
+                node.caption = node.caption.substr(node.caption.indexOf(" ") + 1);
+                valids.banner = node;
+                break;
+            case requiredHastags[6]:
+                valids.gallery.push(node);
+                break;
+            case requiredHastags[8]:
+                valids.is_comment = true;
+                valids.comments = node;
+                break;
+            default:
+                let num = ht.match(/\d+/g);
+                ht = ht.replace(num, "");
+                num = parseInt(num);
+                if(Number.isInteger(num) && (ht === requiredHastags[7])){
+                    node.title = node.caption.split(" ")[0]
+                    valids.stories[num] = node;
+                }
+                break;
+        }
+    });
+
+    valids.stories = valids.stories.filter(item => item !== null);
+
+    // validate theme
+    let _configs = profile.biography.split(" ");
+    let htconfigs = [];
+    _configs.map(c => {
+        if (c.includes('#')) htconfigs.push(c);
+    });
+
+    let configs = {};
+
+    htconfigs.map(ht => {
+        if (ht === '#invikard') {
+            configs.key = ht;
+            profile.biography = profile.biography.replace(ht, "").trim();
+        }
+        if (ht === '#wedding') {
+            configs.type = ht;
+            profile.biography = profile.biography.replace(ht, "").trim();
+        }
+        if (ht === '#1') {
+            configs.theme = ht;
+            profile.biography = profile.biography.replace(ht, "").trim();
+        }
+    });
+
+    if (configs.key && configs.type && configs.theme)
+        valids.is_config = true;
+
+    // check date uri
+
+    if(profile.biography && valids.date && valids.place && valids.banner){
+        valids.date.date_url = `
+        https://www.google.com/calendar/render?action=TEMPLATE&text=${profile.biography}&details=${valids.banner.caption}&location=${valids.place.map_url}&dates=${valids.date.start}/${valids.date.end}
+        `.trimLeft().trimRight();
+        valids.date.date_url = valids.date.date_url.replace(/\n/g, " ")
+    }
+
+    return Object.assign(configs, profile, valids);
+};
