ddeaff72b2c7ceeaaa274a104e7882f9511aae28
hynra <hynra@yahoo.com>
2020-02-15 18:59:01 +0700

    Template engine with `mustache`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Render('index.mustache')
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  render() {
+    const message = this.appService.getHello();
+    return { message };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as mustacheExpress from 'mustache-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  app.engine('mustache', mustacheExpress());
+  app.set('view engine', 'mustache');
+
   await app.listen(3000);
 }
 bootstrap();
